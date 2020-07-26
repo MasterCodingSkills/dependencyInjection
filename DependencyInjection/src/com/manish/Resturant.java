8b60d5dfe14714c90381566d69312e3d93e41a93
MasterCodingSkills <56904368+MasterCodingSkills@users.noreply.github.com>
2020-07-26 17:54:51 +0530

    Tea Dependency of Resturant class is removed. Now dependecy can be passed either by constructor or by setter method.

--- DependencyInjection/src/com/manish/Resturant.java
+++ DependencyInjection/src/com/manish/Resturant.java
@@ -2,8 +2,24 @@ package com.manish;
 
 public class Resturant {
 	//Tea tea = new Tea();
-	IHotBeverage iHotBeverage;
+	private IHotBeverage iHotBeverage;
 	
+	public Resturant() {}
+	
+	// dependency injection by constructor
+	public Resturant(IHotBeverage iHotBeverage) {
+		this.iHotBeverage = iHotBeverage;
+	}
+	
+	public IHotBeverage getiHotBeverage() {
+		return iHotBeverage;
+	}
+	
+	// dependency injection by setter method
+	public void setiHotBeverage(IHotBeverage iHotBeverage) {
+		this.iHotBeverage = iHotBeverage;
+	}
+
 	public void prepareTea() {
 		iHotBeverage.prepareTea();
 	}
--- DependencyInjection/src/com/manish/TestClass.java
+++ DependencyInjection/src/com/manish/TestClass.java
@@ -2,7 +2,20 @@ package com.manish;
 
 public class TestClass {
 	public static void main(String[] args) {
-		Resturant resturant = new Resturant();
-		resturant.prepareTea();
+		
+		// Now either we can use setter method for dependency injection
+		// or we can use constructor way.
+		
+		// 1st setter method way.
+		IHotBeverage tea = new Tea();
+		Resturant resturant1 = new Resturant();
+		resturant1.setiHotBeverage(tea);
+		resturant1.prepareTea();
+		
+		
+		// 2nd constructor way
+		Resturant resturant2 = new Resturant(new Tea());
+		resturant2.prepareTea();
+		
 	}
 }
